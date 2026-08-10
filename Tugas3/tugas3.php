634ce50aed564518ad120a955c1e002388c7093e
novinurkhaeni <novinurkhaeni.1@gmail.com>
2026-08-10 10:24:17 +0700

    Tugas 3 PHP Dasar

--- Tugas3/tugas3.php
+++ Tugas3/tugas3.php
@@ -1,5 +1,5 @@
 <?php
-
+// Upload file jawaban di link berikut https://forms.gle/piT3cCNKUJQfGZyN9
 // SOAL 1 - PRE-INCREMENT. APAKAH ADA YANG ERROR? PERBAIKI JIKA ADA!
 $angka = 15;
 echo "Soal 1: " . ++angka . "<br>";
